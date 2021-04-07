bba6b50cca31f3e52a9ea3e58f55aaaa4d2df6c0
Lauriane <lauriane.haydari@gmail.com>
2021-04-07 11:53:32 +0200

    SUT below five seconds trigger prepare player

--- ProteoEngine/ProteoEngineTests/SoundUseCaseTests.swift
+++ ProteoEngine/ProteoEngineTests/SoundUseCaseTests.swift
@@ -21,6 +21,14 @@ class SoundUseCaseTests: XCTestCase {
         XCTAssertEqual(player.playCallCount, 0)
     }
     
+    func test_belowFiveSeconds_triggerPreparePlayer() {
+        let player = PrepareTrackPlayer()
+        let sut = NotifiesPlayerSerieStart(preparePlayer: player)
+        
+        sut.play(time: 4)
+        
+        XCTAssertEqual(player.playCallCount, 1)
+    }
     
     class PrepareTrackPlayer: TrackPlayer {
         var playCallCount = 0
